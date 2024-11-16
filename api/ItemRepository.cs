24386ba17cfdfdd28f50008ae241a19d7631a033
Dilorfin <pavel.kuybida@gmail.com>
2024-11-17 01:32:33 +0200

    Add API

---
+++ api/ItemRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+ 
+namespace api;
+
+public class ItemRepository
+{
+    private static readonly string ConnectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+
+    private const string DatabaseId = "home-warehouse";
+    private const string ContainerId = "items";
+    private const string PartitionKey = "/id";
+    
+    // The Cosmos client instance
+    private readonly CosmosClient _cosmosClient = new(ConnectionString);
+
+    // The database we will create
+    private Database _database;
+
+    // The container we will create.
+    private Container _container;
+
+    public async Task<bool> ContainsAsync(string id)
+    {
+        var queryable = _container.GetItemLinqQueryable<ItemModel>();
+        var matches = queryable.Where(e => e.Id == id);
+        using FeedIterator<ItemModel> linqFeed = matches.ToFeedIterator();
+        while (linqFeed.HasMoreResults)
+        {
+            var response = await linqFeed.ReadNextAsync();
+            return response.Count > 0;
+        }
+        return false;
+    }
+
+    public async Task<ItemModel> UpsertAsync(ItemModel item)
+    {
+        await Initialize();
+
+        return await _container.UpsertItemAsync(item);
+    }
+    public async Task<ItemModel> GetAsync(string id)
+    {
+        await Initialize();
+
+        try
+        {
+            return await _container.ReadItemAsync<ItemModel>(id, new PartitionKey(id));
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+    public async Task<ItemModel> DeleteAsync(ItemModel item)
+    {
+        await Initialize();
+        
+        return await _container.DeleteItemAsync<ItemModel>(item.Id ,new PartitionKey(item.Id));
+    }
+
+    private async Task Initialize()
+    {
+        _database ??= await _cosmosClient.CreateDatabaseIfNotExistsAsync(DatabaseId);
+        _container ??= await _database.CreateContainerIfNotExistsAsync(ContainerId, PartitionKey);
+    }
+}
--- api/UpsertItem.cs
+++ api/UpsertItem.cs
@@ -1,28 +1,28 @@
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace api;
 
-public static class GetItem
+public static class UpsertItem
 {
-    [FunctionName("GetItem")]
+    [FunctionName("UpsertItem")]
     public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+        [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
         ILogger log)
     {
         var repo = new ItemRepository();
 
         log.LogInformation("C# HTTP trigger function processed a request.");
+        
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        var data = JsonConvert.DeserializeObject<ItemModel>(requestBody);
 
-        string id = req.Query["id"];
-
-        var result = await repo.GetAsync(id);
-        if (result is null)
-            return new NotFoundResult();
-        return new OkObjectResult(result);
+        return new OkObjectResult(await repo.UpsertAsync(data));
     }
 }
