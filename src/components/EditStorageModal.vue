c4d5194f7bbc61cb84c1933651c13e9310b9e0b6
Dilorfin <dilorfin@duck.com>
2024-11-20 19:37:37 +0200

    Trying to make some component

---
+++ src/components/EditStorageModal.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { StorageModel, ItemModel } from '@/models/StorageModel';
+
+const storageData = defineModel<StorageModel>('storageData');
+const itemData = defineModel<ItemModel>('itemData');
+
+console.log(itemData.id);
+</script>
+
+<template>
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Edit item</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="id-modal-addon">ID</span>
+            <input type="text" class="form-control" aria-describedby="id-modal-addon" v-model="itemData.id" >
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="count-modal-addon">Count</span>
+            <input type="text" class="form-control" aria-describedby="count-modal-addon" v-model="itemData.count">
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="title-modal-addon">Title</span>
+            <input type="text" class="form-control" aria-describedby="title-modal-addon" v-model="itemData.title">
+          </div>
+          <div class="input-group">
+            <span class="input-group-text">Comment</span>
+            <textarea class="form-control" v-model="itemData.comment"></textarea>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-success"><i class="bi bi-check"></i> Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/StorageView.vue
+++ src/views/StorageView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { StorageModel } from '@/models/StorageModel';
+import type { StorageModel, ItemModel } from '@/models/StorageModel';
 
 import QrcodeVue from 'qrcode.vue';
 import { ref, watch } from 'vue';
@@ -11,6 +11,7 @@ const storageId = ref(Array.isArray(route?.params?.id) ? route?.params?.id[0] : 
 const isLoading = ref(true);
 const storageExists = ref(false);
 const storageData = ref({} as StorageModel);
+const itemEditData = ref({} as ItemModel);
 
 fetch("/api/GetStorage/?id=" + storageId.value).then(async (response: Response)=>{
 
@@ -121,38 +122,7 @@ watch(
       </div>
     </div>
   </div>
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="id-modal-addon">ID</span>
-            <input type="text" class="form-control" aria-describedby="id-modal-addon">
-          </div>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="count-modal-addon">Count</span>
-            <input type="text" class="form-control" aria-describedby="count-modal-addon">
-          </div>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="title-modal-addon">Title</span>
-            <input type="text" class="form-control" aria-describedby="title-modal-addon">
-          </div>
-          <div class="input-group">
-            <span class="input-group-text">Comment</span>
-            <textarea class="form-control"></textarea>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-success"><i class="bi bi-check"></i> Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div>
+  <EditStorageModal v-model:storageData="storageData" v-model:itemData="itemEditData"></EditStorageModal>
 </template>
 
 <style scoped>
