bc5ca623f1dd117a1c0436e8ebfe69db28e6a9c6
dilorfin <dilorfin@duck.com>
2024-11-20 19:05:48 +0200

    Add icons package

--- src/views/StorageView.vue
+++ src/views/StorageView.vue
@@ -154,7 +154,7 @@ watch(
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-success"><i class="bi bi-check"></i> Save changes</button>
         </div>
       </div>
     </div>
