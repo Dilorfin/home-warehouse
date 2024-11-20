ed2a80b3cbc109bb000773cc0945e39bcd017723
Dilorfin <pavel.kuybida@gmail.com>
2024-11-20 12:36:59 +0200

    Add some more styles to items page

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -5,15 +5,23 @@ import { useRoute } from 'vue-router'
 
 const route = useRoute()
 
-const size :number = 300;
-
 const itemId = ref(Array.isArray(route?.params?.id) ? route?.params?.id[0] : route?.params?.id);
 const isLoading = ref(true);
 const itemExists = ref(false);
 const data = ref("");
 
 fetch("/api/GetItem/?id=" + itemId.value).then(async (response: Response)=>{
-  console.log(response);
+
+  isLoading.value = false;
+  itemExists.value = false;
+
+  if (!response.bodyUsed)
+  {
+    //itemExists.value = true;
+    //data.value = "Якісь данні";
+    return;
+  }
+
   let result = await response.json();
   if (response.ok)
   {
@@ -22,12 +30,11 @@ fetch("/api/GetItem/?id=" + itemId.value).then(async (response: Response)=>{
   }
   else
   {
-    itemExists.value = false;
     //data.value = result["error"];
   }
-  isLoading.value = false;
 });
-/*watch(
+/* ???
+watch(
   () => route.params.id,
   (newId, oldId) => {
     itemId.value = Array.isArray(newId) ? newId[0] : newId;
@@ -37,17 +44,34 @@ fetch("/api/GetItem/?id=" + itemId.value).then(async (response: Response)=>{
 </script>
 
 <template>
-  <h3>Item: {{ itemId }}</h3>
-  <div v-if="isLoading" class="spinner-border text-primary" role="status">
-    <span class="visually-hidden">Loading...</span>
-  </div>
-  <div v-if="itemExists">
-    <p>{{ data }}</p>
-    <qrcode-vue :value="itemId" :size="size"></qrcode-vue>
-  </div>
-  <div v-else-if="!itemExists">
-    <p>{{ data }}</p>
-    <qrcode-vue :value="itemId" :size="size"></qrcode-vue>
+  <div class="row">
+    <div class="col-8">
+      <div v-if="isLoading" class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+      <div v-else-if="!itemExists" class="alert alert-primary" role="alert">
+        No data found
+        <a class="btn btn-primary">AAA</a>
+      </div>
+      <div v-else>
+        {{ data }}
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Item: {{ itemId }}</h3>
+        </div>
+        <div class="card-body">
+          <div v-if="itemExists">
+            <qrcode-vue :value="itemId" :size="100"></qrcode-vue>
+          </div>
+          <a class="btn btn-outline-primary">Download QR</a>
+          <a class="btn btn-outline-primary">Print QR</a>
+        </div>
+        
+      </div>
+    </div>
   </div>
 </template>
 
