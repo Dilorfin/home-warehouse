469805cb2be217513defb34a9c58871f051b2b6a
Dilorfin <dilorfin@duck.com>
2024-11-20 20:03:28 +0200

    Remove component from the way

--- src/views/StorageView.vue
+++ src/views/StorageView.vue
@@ -125,7 +125,38 @@ function openEdit(item:ItemModel)
       </div>
     </div>
   </div>
-  <EditStorageModal v-model:storageData="storageData" v-model:itemData="itemEditData"></EditStorageModal>
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Edit item</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="id-modal-addon">ID</span>
+            <input type="text" class="form-control" aria-describedby="id-modal-addon" v-model="itemEditData.id" >
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="count-modal-addon">Count</span>
+            <input type="text" class="form-control" aria-describedby="count-modal-addon" v-model="itemEditData.count">
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="title-modal-addon">Title</span>
+            <input type="text" class="form-control" aria-describedby="title-modal-addon" v-model="itemEditData.title">
+          </div>
+          <div class="input-group">
+            <span class="input-group-text">Comment</span>
+            <textarea class="form-control" v-model="itemEditData.comment"></textarea>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-success"><i class="bi bi-check"></i> Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
