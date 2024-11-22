cf676ec479959c6bf64b630404a8a11e7ee52995
Dilorfin <dilorfin@duck.com>
2024-11-22 19:30:14 +0200

    Add not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import StorageView from '@/views/StorageView.vue'
 import OpenStorageView from '@/views/OpenStorageView.vue'
 import StoragesListView from '@/views/SearchView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/storage/:id',
       name: 'storage',
       component: StorageView,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFoundView
     }
   ],
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<h3>Page not found</h3>
+</template>
+
+<style scoped>
+
+</style>
